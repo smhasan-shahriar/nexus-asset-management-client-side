839dbef864a89bf91fd1f15a234d2fc95bd7b45e
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-23 19:07:14 +0600

    created basic react router, installed react router, tailwind

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layouts/MainLayout';
+import Home from '../Pages/Home/Home';
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+      ]
+    },
+  ]);
+
